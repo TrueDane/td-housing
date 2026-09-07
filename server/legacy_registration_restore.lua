e1faf240cc4e1afaf8464004986db024b72b3924
True Dane Development <80951720+TrueDane@users.noreply.github.com>
2026-09-07 21:57:55 +0200

    fix: restore runtime registration functions

---
+++ server/legacy_registration_restore.lua
@@ -0,0 +1,8 @@
+local LegacyRegistration = TDHousing and TDHousing.LegacyRegistration
+
+if not LegacyRegistration then
+    return
+end
+
+RegisterNetEvent = LegacyRegistration.registerNetEvent
+lib.callback.register = LegacyRegistration.callbackRegister
