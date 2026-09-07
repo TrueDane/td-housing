36662702abfd14f58cb7fe0fc52ded91f3718cef
True Dane Development <80951720+TrueDane@users.noreply.github.com>
2026-09-07 21:47:45 +0200

    refactor: establish td housing runtime foundation

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,46 +1,55 @@
 fx_version 'cerulean'
+game 'gta5'
 
-game "gta5"
-
-author "Xirvin#0985 and Project Sloth"
-version '2.0.8'
-
-repository 'Project-Sloth/ps-housing'
-
-lua54 'yes'
+author 'TrueDane Development'
+description 'TrueDane 3.0 housing engine based on ps-housing'
+version '0.1.0-dev'
+repository 'https://github.com/TrueDane/td-housing'
 
 ui_page 'html/index.html'
 
-dependency {
-  'fivem-freecam',
+dependencies {
+    'td_bridge',
+    'ox_lib',
+    'oxmysql',
+    'fivem-freecam',
 }
 
-shared_script {
-  '@ox_lib/init.lua',
-  "shared/config.lua",
-  "shared/framework.lua",
+shared_scripts {
+    '@td_bridge/init.lua',
+    '@ox_lib/init.lua',
+    'shared/config.lua',
+    'shared/framework.lua',
+    'shared/property_capabilities.lua',
 }
 
-client_script {
-  'client/shell.lua',
-  'client/apartment.lua',
-  'client/cl_property.lua',
-  'client/client.lua',
-  'client/modeler.lua',
-  'client/migrate.lua'
+client_scripts {
+    'config/client.lua',
+    'client/shell.lua',
+    'client/apartment.lua',
+    'client/cl_property.lua',
+    'client/client.lua',
+    'client/modeler.lua',
+    'client/integrations/wardrobe.lua',
+    'client/modules/property_capabilities.lua',
+    'client/migrate.lua',
 }
 
-server_script {
-  '@oxmysql/lib/MySQL.lua',
-  "server/sv_property.lua",
-  "server/server.lua",
-  "server/migrate.lua",
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/sv_property.lua',
+    'server/repositories/property_repository.lua',
+    'server/services/property_capability_service.lua',
+    'server/services/legacy_guard_service.lua',
+    'server/modules/property_capabilities.lua',
+    'server/server.lua',
+    'server/migrate.lua',
 }
 
 files {
-  'html/**',
-  'stream/starter_shells_k4mb1.ytyp',
-  'stream/**.ytyp'
+    'html/**',
+    'stream/starter_shells_k4mb1.ytyp',
+    'stream/**.ytyp',
 }
 
 -- Fix for "stuck in black loading screen"
@@ -50,7 +59,6 @@ data_file 'DLC_ITYP_REQUEST' 'x64c:/levels/gta5/interiors/int_props/int_lev_des.
 data_file 'DLC_ITYP_REQUEST' 'x64c:/levels/gta5/interiors/int_props/int_residential.rpf/int_residential.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'x64c:/levels/gta5/interiors/int_props/int_retail.rpf/int_retail.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'x64c:/levels/gta5/interiors/int_props/int_services.rpf/int_services.ytyp'
-
 data_file 'DLC_ITYP_REQUEST' 'starter_shells_k4mb1.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/**.ytyp'
 
