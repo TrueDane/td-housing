b262176d39703842ce3ffe510bf044f3467d5c05
True Dane Development <80951720+TrueDane@users.noreply.github.com>
2026-09-10 22:53:25 +0200

    style: format housing ownership service

--- server/services/ownership_service.lua
+++ server/services/ownership_service.lua
@@ -9,13 +9,13 @@ local function normalizeIdentifier(identifier)
 		return nil
 	end
 
-	if type(identifier) ~= 'string' then
+	if type(identifier) ~= "string" then
 		return false
 	end
 
-	identifier = identifier:match('^%s*(.-)%s*$')
+	identifier = identifier:match("^%s*(.-)%s*$")
 
-	if identifier == '' then
+	if identifier == "" then
 		return false
 	end
 
@@ -26,13 +26,13 @@ function OwnershipService.SetOwner(propertyId, identifier)
 	local property = Property.Get(propertyId)
 
 	if not property then
-		return nil, 'PROPERTY_NOT_FOUND'
+		return nil, "PROPERTY_NOT_FOUND"
 	end
 
 	identifier = normalizeIdentifier(identifier)
 
 	if identifier == false then
-		return nil, 'INVALID_IDENTIFIER'
+		return nil, "INVALID_IDENTIFIER"
 	end
 
 	local previousOwner = property.propertyData.owner
@@ -42,21 +42,21 @@ function OwnershipService.SetOwner(propertyId, identifier)
 	end
 
 	if not PropertyRepository.UpdateOwner(property.property_id, identifier) then
-		return nil, 'OWNER_UPDATE_FAILED'
+		return nil, "OWNER_UPDATE_FAILED"
 	end
 
-	if previousOwner and property.propertyData.shell == 'mlo' then
+	if previousOwner and property.propertyData.shell == "mlo" then
 		property:removeMloDoorsAccess(previousOwner)
 	end
 
 	property.propertyData.owner = identifier
 
-	if identifier and property.propertyData.shell == 'mlo' then
+	if identifier and property.propertyData.shell == "mlo" then
 		property:addMloDoorsAccess(identifier)
 	end
 
-	TriggerClientEvent('ps-housing:client:updateProperty', -1, 'UpdateOwner', property.property_id, identifier)
-	TriggerClientEvent('td_housing:client:ownerChanged', -1, property.property_id, identifier)
+	TriggerClientEvent("ps-housing:client:updateProperty", -1, "UpdateOwner", property.property_id, identifier)
+	TriggerClientEvent("td_housing:client:ownerChanged", -1, property.property_id, identifier)
 
 	return true
 end
