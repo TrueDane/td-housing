128467d0f18af51d7bf616c0dd50ebfa6baca9ea
True Dane Development <80951720+TrueDane@users.noreply.github.com>
2026-09-10 22:53:32 +0200

    style: format housing public API

--- server/modules/public_api.lua
+++ server/modules/public_api.lua
@@ -2,7 +2,7 @@ local PropertyCapabilityService = TDHousing.PropertyCapabilityService
 local OwnershipService = TDHousing.OwnershipService
 
 local function copySettings(propertyData)
-	if type(propertyData) ~= 'table' then
+	if type(propertyData) ~= "table" then
 		return nil
 	end
 
@@ -21,34 +21,34 @@ local function copySettings(propertyData)
 	return nil
 end
 
-exports('RegisterProperty', function(propertyData, preventEnter, playerSource)
-	if type(propertyData) ~= 'table' then
-		return nil, 'INVALID_PROPERTY_DATA'
+exports("RegisterProperty", function(propertyData, preventEnter, playerSource)
+	if type(propertyData) ~= "table" then
+		return nil, "INVALID_PROPERTY_DATA"
 	end
 
 	local settings = copySettings(propertyData)
 	local propertyId = RegisterProperty(propertyData, preventEnter, playerSource)
 
 	if not propertyId then
-		return nil, 'PROPERTY_CREATE_FAILED'
+		return nil, "PROPERTY_CREATE_FAILED"
 	end
 
 	if settings then
 		local success, errorCode = PropertyCapabilityService.ApplySettings(propertyId, settings)
 
 		if not success then
-			return nil, errorCode or 'PROPERTY_SETTINGS_FAILED'
+			return nil, errorCode or "PROPERTY_SETTINGS_FAILED"
 		end
 	end
 
 	return tostring(propertyId)
 end)
 
-exports('SetOwner', function(propertyId, identifier)
+exports("SetOwner", function(propertyId, identifier)
 	return OwnershipService.SetOwner(propertyId, identifier)
 end)
 
-exports('GetProperty', function(propertyId)
+exports("GetProperty", function(propertyId)
 	local property = Property.Get(propertyId)
 
 	return property and property.propertyData or nil
